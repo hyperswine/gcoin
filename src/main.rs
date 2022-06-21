c2a1585f2e05a0248084f33a265ecde86de1b17b
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-06-21 21:20:22 +1000

    Add frontend skel

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
-fn main() {
-    /*
-    GCOIN
+use argh::FromArgs;
+use std::{error::Error, time::Duration};
 
-    1. pay someone => broadcast this out
-    2. take a broadcast => update blockchain
+mod app;
+mod crossterm;
+mod ui;
 
-    Interface
+/// Demo
+#[derive(Debug, FromArgs)]
+struct Cli {
+    /// time in ms between two ticks.
+    #[argh(option, default = "250")]
+    tick_rate: u64,
+    /// whether unicode symbols are used to improve the overall look of the app
+    #[argh(option, default = "true")]
+    enhanced_graphics: bool,
+}
 
-    - TUI for quick and simple wallet/address management
-    - quickly pay someone by pasting their address and incrementing the amount of coins you want to pay them
-    - receive notifications when the blockchain changes. Especially if you get paid or your payment seems to have been accepted by a lot of people
-    */
-
-    // if option --generate, generate a key pair and place them in ~/.gcoin/rsa.pub and ~/.gcoin/rsa.priv
-
-    // if option --send, broadcast signal via the gcoin port 69000. All N nodes (network nodes) connected to your system should receive that signal from that socket
-    // since you should have socket.bind() to them already
-
-    // if option --find, find N nodes near you and attempt to create and bind sockets to them. If at least one accepts, send a test message to them
-
-    // NOTE: you are also technically a 'server' for the network. The network nodes also broadcast new stuff to you. If you arent online, it wont send it. But when you do go online, it will send all pending changes to you
-    // multiple nodes may send the same changes. Or if one of them is out of date, etc. it may send an older or newer copy of the blockchain. Always follow the longest chain and the blockchain that most nodes are on
-    // have a reference count for each 'acceptable' blockchain
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli: Cli = argh::from_env();
+    let tick_rate = Duration::from_millis(cli.tick_rate);
+    crossterm::run(tick_rate, cli.enhanced_graphics)?;
+    Ok(())
 }
